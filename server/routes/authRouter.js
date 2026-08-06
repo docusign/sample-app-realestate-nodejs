c29655c00c1b8f9911c338b1e834cd7435f6257f
RomanBachaloSigmaSoftware <roman.bachalo@sigma.software>
2026-08-06 16:19:32 +0300

    add CSRF protection and server-side sessions to login

--- server/routes/authRouter.js
+++ server/routes/authRouter.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const csrf = require('csurf');
 const authController = require('../controllers/authController');
 
-router.get('/login', authController.login);
+const csrfProtection = csrf({ cookie: false }); // secret stored in cookie-session
+
+router.get('/csrf-token', csrfProtection, authController.getCsrfToken);
+router.post('/login', csrfProtection, authController.login);
 router.get('/login/callback', (req, res) => res.send("hi"));
 module.exports = router;
